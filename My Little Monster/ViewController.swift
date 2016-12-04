0791c825381797cf4d3d02e18ac43344937dc925
Eduardo <e.chiaro@gmail.com>
2016-12-03 16:33:06 -0800

    Initial Commit

---
+++ My Little Monster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Little Monster
+//
+//  Created by Eduardo Chiaro on 12/3/16.
+//  Copyright © 2016 Eduardo Chiaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
